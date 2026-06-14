a79a5aeab9a946edd25a9dd130e80d3433f23dbe
Lucas Silva <lucassiso81@gmail.com>
2026-06-14 00:43:08 -0300

    Spring project created

---
+++ src/main/java/com/learningproject/xablaux/XablauxApplication.java
@@ -0,0 +1,13 @@
+package com.learningproject.xablaux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XablauxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XablauxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningproject/xablaux/XablauxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningproject.xablaux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XablauxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
