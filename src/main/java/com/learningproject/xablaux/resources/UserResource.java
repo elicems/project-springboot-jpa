78e516b52a11e00b92e5ebc55300d09447b457a5
Lucas Silva <lucassiso81@gmail.com>
2026-06-16 23:08:55 -0300

    Service layer implemented, component registration

--- src/main/java/com/learningproject/xablaux/resources/UserResource.java
+++ src/main/java/com/learningproject/xablaux/resources/UserResource.java
@@ -1,16 +1,31 @@
 package com.learningproject.xablaux.resources;
 
 import com.learningproject.xablaux.entities.User;
+import com.learningproject.xablaux.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"Marcola","pcc1533@gmail.com","7777777777","qwerty8");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
