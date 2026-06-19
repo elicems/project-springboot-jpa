ca60e7bf4d1fcbf6faad852ba571e00cddfc101c
Lucas Silva <lucassiso81@gmail.com>
2026-06-19 20:41:51 -0300

    Class enum OrderStatus implemented

--- src/main/java/com/learningproject/xablaux/entities/Order.java
+++ src/main/java/com/learningproject/xablaux/entities/Order.java
@@ -1,6 +1,7 @@
 package com.learningproject.xablaux.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.learningproject.xablaux.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,12 +21,15 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    private Integer orderStatus;
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -51,6 +55,14 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     @Override
