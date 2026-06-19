76ada4ae44ff2a0c0d0e3a51a34d2a8ed03fe350
Lucas Silva <lucassiso81@gmail.com>
2026-06-18 23:00:43 -0300

    Created Order entity, repository, resource and database seeding

---
+++ src/main/java/com/learningproject/xablaux/services/OrderService.java
@@ -0,0 +1,23 @@
+package com.learningproject.xablaux.services;
+
+import com.learningproject.xablaux.entities.Order;
+import com.learningproject.xablaux.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+    public Order findById(Long id){
+        Optional<Order> obj = orderRepository.findById(id);
+        return obj.get();
+    }
+}
