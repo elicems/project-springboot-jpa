3db47feed082e2db7280ada07fd238b99d3dc1ec
Lucas Silva <lucassiso81@gmail.com>
2026-06-20 23:56:42 -0300

    Entity Product created

--- src/main/java/com/learningproject/xablaux/configuration/ConfigTest.java
+++ src/main/java/com/learningproject/xablaux/configuration/ConfigTest.java
@@ -2,10 +2,12 @@ package com.learningproject.xablaux.configuration;
 
 import com.learningproject.xablaux.entities.Category;
 import com.learningproject.xablaux.entities.Order;
+import com.learningproject.xablaux.entities.Product;
 import com.learningproject.xablaux.entities.User;
 import com.learningproject.xablaux.entities.enums.OrderStatus;
 import com.learningproject.xablaux.repositories.CategoryRepository;
 import com.learningproject.xablaux.repositories.OrderRepository;
+import com.learningproject.xablaux.repositories.ProductRepository;
 import com.learningproject.xablaux.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -27,6 +29,9 @@ public class ConfigTest implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null,"Zé ruela","ruela@gmail.com","98888888","123456");
@@ -45,6 +50,14 @@ public class ConfigTest implements CommandLineRunner {
         Category cat3 = new Category(null, "Computers");
 
         categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
     }
 
 }
---
+++ src/main/java/com/learningproject/xablaux/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.learningproject.xablaux.services;
+
+import com.learningproject.xablaux.entities.Product;
+import com.learningproject.xablaux.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+    public Product findById(Long id){
+        Optional<Product> obj = productRepository.findById(id);
+        return obj.get();
+    }
+}
