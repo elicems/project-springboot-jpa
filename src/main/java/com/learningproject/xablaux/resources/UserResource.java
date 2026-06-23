33d96787454d14b904abfa5afaadf4773f678575
Lucas Silva <lucassiso81@gmail.com>
2026-06-22 22:40:41 -0300

    Delete user created

--- src/main/java/com/learningproject/xablaux/resources/UserResource.java
+++ src/main/java/com/learningproject/xablaux/resources/UserResource.java
@@ -34,4 +34,9 @@ public class UserResource {
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
