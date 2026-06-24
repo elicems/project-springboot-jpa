7caa9138e69ba9dfeed9c30ef88feeec229529d0
Lucas Silva <lucassiso81@gmail.com>
2026-06-23 22:39:01 -0300

    findById exception

--- src/main/java/com/learningproject/xablaux/entities/Order.java
+++ src/main/java/com/learningproject/xablaux/entities/Order.java
@@ -17,7 +17,7 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT")
     private Instant moment;
 
     @ManyToOne
